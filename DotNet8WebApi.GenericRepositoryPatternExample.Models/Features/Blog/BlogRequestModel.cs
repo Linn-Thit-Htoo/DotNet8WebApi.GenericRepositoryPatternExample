05b196f391f6634a1105c4043e94784a9a859487
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:40:11 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Blog/BlogRequestModel.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Blog/BlogRequestModel.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog;
 
-namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string? BlogTitle { get; set; } = null!;
+    public string? BlogTitle { get; set; } = null!;
 
-        public string? BlogAuthor { get; set; } = null!;
+    public string? BlogAuthor { get; set; } = null!;
 
-        public string? BlogContent { get; set; } = null!;
-    }
+    public string? BlogContent { get; set; } = null!;
 }
