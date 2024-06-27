2e73bf3042d5e3a946b56211e17fd6d0a7c8c46d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:36:19 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Enums/EnumStatusCode.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Enums/EnumStatusCode.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Enums
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Duplicate = 409,
-        InternalServerError = 500
-    }
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Duplicate = 409,
+    InternalServerError = 500
 }
