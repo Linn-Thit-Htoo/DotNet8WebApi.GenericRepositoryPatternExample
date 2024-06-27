457d01f11b6dabbad5ed4af08e4562b8d3f7a68e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:36:29 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Result.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Result.cs
@@ -1,11 +1,4 @@
 ﻿using DotNet8WebApi.GenericRepositoryPatternExample.Models.Enums;
-using DotNet8WebApi.GenericRepositoryPatternExample.Models.Resources;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Features
 {
