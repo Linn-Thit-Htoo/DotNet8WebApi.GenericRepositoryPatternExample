54d6791b51c935ebadf901a41755708667af2ef8
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:37:31 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Repositories/Features/GenericRepository/IGenericRepository.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Repositories/Features/GenericRepository/IGenericRepository.cs
@@ -1,14 +1,13 @@
 ﻿using DotNet8WebApi.GenericRepositoryPatternExample.Models.Features;
 
-namespace DotNet8WebApi.GenericRepositoryPatternExample.Repositories.Features.GenericRepository
+namespace DotNet8WebApi.GenericRepositoryPatternExample.Repositories.Features.GenericRepository;
+
+public interface IGenericRepository<T> where T : class
 {
-    public interface IGenericRepository<T> where T : class
-    {
-        Task<Result<T>> GetListAsync();
-        Task<Result<T>> GetListOrderByDescAsync<TKey>(Func<T, TKey> orderBySelector);
-        Task<Result<T>> GetByIdAsync(int id);
-        Task<Result<T>> AddAsync(T requestModel);
-        Task<Result<T>> UpdateAsync(T requestModel, int id);
-        Task<Result<T>> DeleteAsync(int id);
-    }
+    Task<Result<T>> GetListAsync();
+    Task<Result<T>> GetListOrderByDescAsync<TKey>(Func<T, TKey> orderBySelector);
+    Task<Result<T>> GetByIdAsync(int id);
+    Task<Result<T>> AddAsync(T requestModel);
+    Task<Result<T>> UpdateAsync(T requestModel, int id);
+    Task<Result<T>> DeleteAsync(int id);
 }
