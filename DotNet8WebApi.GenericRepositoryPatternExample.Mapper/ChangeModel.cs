79479fb1e650fa8efab587b801fc3462153ae394
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:36:03 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Mapper/ChangeModel.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Mapper/ChangeModel.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet8WebApi.GenericRepositoryPatternExample.DbService.AppDbContexts;
 using DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog;
 
-namespace DotNet8WebApi.GenericRepositoryPatternExample.Mapper
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Change(this TblBlog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+namespace DotNet8WebApi.GenericRepositoryPatternExample.Mapper;
 
-        public static TblBlog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Change(this TblBlog dataModel)
+    {
+        return new BlogModel
         {
-            return new TblBlog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static TblBlog Change(this BlogRequestModel requestModel)
+    {
+        return new TblBlog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
