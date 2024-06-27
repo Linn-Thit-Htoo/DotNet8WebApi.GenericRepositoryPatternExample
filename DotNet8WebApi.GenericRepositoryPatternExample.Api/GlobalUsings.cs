9893fb3e02a2bb094ade849a1688769796f6b30d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:33:06 +0630

    Added Global Usings

---
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using DotNet8WebApi.GenericRepositoryPatternExample.DbService.AppDbContexts;
+global using DotNet8WebApi.GenericRepositoryPatternExample.Mapper;
+global using DotNet8WebApi.GenericRepositoryPatternExample.Models.Features;
+global using DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog;
+global using DotNet8WebApi.GenericRepositoryPatternExample.Models.Resources;
+global using DotNet8WebApi.GenericRepositoryPatternExample.Repositories.Features.GenericRepository;
