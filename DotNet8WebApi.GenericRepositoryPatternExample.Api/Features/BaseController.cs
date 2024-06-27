3a20e3b49ec9a3ca2ff833a5dbeedf74ce3d7591
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:34:21 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/BaseController.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/BaseController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace DotNet8WebApi.GenericRepositoryPatternExample.Api.Features
