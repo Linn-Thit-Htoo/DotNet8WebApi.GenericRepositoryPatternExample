260486c90add04560375bab6f2369aacfa4b1d93
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:38:48 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Api/ModularService.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/ModularService.cs
@@ -1,45 +1,44 @@
-﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Api
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Api;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
+        services.AddDbContextService(builder)
+            .AddBusinessLogicService()
+            .AddRepositoryService()
+            .AddJsonService();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContextService(builder)
-                .AddBusinessLogicService()
-                .AddRepositoryService()
-                .AddJsonService();
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>()
+            .AddScoped<BL_Student>();
+        return services;
+    }
 
-            return services;
-        }
+    private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        return services;
+    }
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>()
-                .AddScoped<BL_Student>();
-            return services;
-        }
-
-        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            return services;
-        }
-
-        private static IServiceCollection AddJsonService(this IServiceCollection services)
-        {
-            services.AddControllers();
-            return services;
-        }
+    private static IServiceCollection AddJsonService(this IServiceCollection services)
+    {
+        services.AddControllers();
+        return services;
     }
 }
