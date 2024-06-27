7167eb3cf589f0f7556ab5c5d62948ce335a8c44
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:40:01 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Blog/BlogModel.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Blog/BlogModel.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog;
 
-namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Features.Blog
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
+    public int BlogId { get; set; }
 
-        public string? BlogTitle { get; set; }
+    public string? BlogTitle { get; set; }
 
-        public string? BlogAuthor { get; set; }
+    public string? BlogAuthor { get; set; }
 
-        public string? BlogContent { get; set; }
-    }
+    public string? BlogContent { get; set; }
 }
