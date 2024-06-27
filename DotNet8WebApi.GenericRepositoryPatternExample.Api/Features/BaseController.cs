5e2d0ee66217e9926bfbabf197f7b62ee312f1d0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:34:29 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/BaseController.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/BaseController.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8WebApi.GenericRepositoryPatternExample.Api.Features
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        protected IActionResult Content(object obj)
-        {
-            return Ok(JsonConvert.SerializeObject(obj));
-        }
+namespace DotNet8WebApi.GenericRepositoryPatternExample.Api.Features;
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult Content(object obj)
+    {
+        return Ok(JsonConvert.SerializeObject(obj));
+    }
+
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
