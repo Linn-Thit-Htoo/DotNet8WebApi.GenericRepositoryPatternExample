33bd8364013a4e4e51a3fdf94eefd8c08dc0c54d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:39:05 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Api/Program.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/Program.cs
@@ -2,10 +2,6 @@ using DotNet8WebApi.GenericRepositoryPatternExample.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +9,6 @@ builder.Services.AddFeatures(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
