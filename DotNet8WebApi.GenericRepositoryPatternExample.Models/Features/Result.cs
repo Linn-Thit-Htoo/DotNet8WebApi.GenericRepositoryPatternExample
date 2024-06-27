5a565f8bec140cbad0f8f9d76c6a61d878b49d2a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:43:17 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Result.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Features/Result.cs
@@ -4,28 +4,75 @@ public class Result<T>
 {
     public List<T> Data { get; set; }
     public bool IsSuccess { get; set; }
-    public bool IsError { get { return !IsSuccess; } }
+    public bool IsError
+    {
+        get { return !IsSuccess; }
+    }
     public string Message { get; set; }
     public EnumStatusCode StatusCode { get; set; }
-    public static Result<T> SuccessResult(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+
+    public static Result<T> SuccessResult(
+        string message = "Success.",
+        EnumStatusCode statusCode = EnumStatusCode.Success
+    )
     {
-        return new Result<T> { IsSuccess = true, Message = message, StatusCode = statusCode };
+        return new Result<T>
+        {
+            IsSuccess = true,
+            Message = message,
+            StatusCode = statusCode
+        };
     }
-    public static Result<T> SuccessResult(List<T> data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+
+    public static Result<T> SuccessResult(
+        List<T> data,
+        string message = "Success.",
+        EnumStatusCode statusCode = EnumStatusCode.Success
+    )
     {
-        return new Result<T> { IsSuccess = true, Message = message, StatusCode = statusCode, Data = data };
+        return new Result<T>
+        {
+            IsSuccess = true,
+            Message = message,
+            StatusCode = statusCode,
+            Data = data
+        };
     }
-    public static Result<T> FailureResult(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+
+    public static Result<T> FailureResult(
+        string message = "Fail.",
+        EnumStatusCode statusCode = EnumStatusCode.BadRequest
+    )
     {
-        return new Result<T> { IsSuccess = false, Message = message, StatusCode = statusCode };
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode
+        };
     }
-    public static Result<T> FailureResult(Exception ex, EnumStatusCode statusCode = EnumStatusCode.InternalServerError)
+
+    public static Result<T> FailureResult(
+        Exception ex,
+        EnumStatusCode statusCode = EnumStatusCode.InternalServerError
+    )
     {
-        return new Result<T> { IsSuccess = false, Message = ex.ToString(), StatusCode = statusCode };
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Message = ex.ToString(),
+            StatusCode = statusCode
+        };
     }
-    public static Result<T> ExecuteResult(int result, EnumStatusCode successStatusCode = EnumStatusCode.Success, EnumStatusCode failureStatusCode = EnumStatusCode.BadRequest)
+
+    public static Result<T> ExecuteResult(
+        int result,
+        EnumStatusCode successStatusCode = EnumStatusCode.Success,
+        EnumStatusCode failureStatusCode = EnumStatusCode.BadRequest
+    )
     {
-        return result > 0 ? Result<T>.SuccessResult(statusCode: successStatusCode)
+        return result > 0
+            ? Result<T>.SuccessResult(statusCode: successStatusCode)
             : Result<T>.FailureResult(statusCode: failureStatusCode);
     }
 }
