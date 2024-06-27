70b6071e37afb437e90a6173b9feb2c2c1552a94
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:33:56 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/Student/BL_Student.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Api/Features/Student/BL_Student.cs
@@ -1,27 +1,26 @@
-﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Api.Features.Student
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Api.Features.Student;
+
+public class BL_Student
 {
-    public class BL_Student
+    private readonly IGenericRepository<TblStudent> _genericRepository;
+
+    public BL_Student(IGenericRepository<TblStudent> genericRepository)
     {
-        private readonly IGenericRepository<TblStudent> _genericRepository;
+        _genericRepository = genericRepository;
+    }
 
-        public BL_Student(IGenericRepository<TblStudent> genericRepository)
+    public async Task<Result<TblStudent>> GetStudentList()
+    {
+        Result<TblStudent> responseModel;
+        try
         {
-            _genericRepository = genericRepository;
+            responseModel = await _genericRepository.GetListOrderByDescAsync(x => x.StudentId);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<TblStudent>.FailureResult(ex);
         }
 
-        public async Task<Result<TblStudent>> GetStudentList()
-        {
-            Result<TblStudent> responseModel;
-            try
-            {
-                responseModel = await _genericRepository.GetListOrderByDescAsync(x => x.StudentId);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<TblStudent>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
+        return responseModel;
     }
 }
