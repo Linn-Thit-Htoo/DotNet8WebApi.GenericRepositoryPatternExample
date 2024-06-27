92cf96ea65300fa56bfa9bb215bebca8aeea5488
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:36:55 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Models/Resources/MessageResource.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Models/Resources/MessageResource.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Resources
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Models.Resources;
+
+public class MessageResource
 {
-    public class MessageResource
-    {
-        public static string Success { get; } = "Success.";
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string UpdateSuccess { get; } = "Updating Successful.";
-        public static string UpdateFail { get; } = "Updating Fail.";
-        public static string DeleteSuccess { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-        public static string NotFound { get; } = "No Data Found.";
-        public static string Duplicate { get; } = "Duplicate Data.";
-        public static string InvalidId { get; } = "Id is invalid.";
-    }
+    public static string Success { get; } = "Success.";
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string UpdateSuccess { get; } = "Updating Successful.";
+    public static string UpdateFail { get; } = "Updating Fail.";
+    public static string DeleteSuccess { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
+    public static string NotFound { get; } = "No Data Found.";
+    public static string Duplicate { get; } = "Duplicate Data.";
+    public static string InvalidId { get; } = "Id is invalid.";
 }
