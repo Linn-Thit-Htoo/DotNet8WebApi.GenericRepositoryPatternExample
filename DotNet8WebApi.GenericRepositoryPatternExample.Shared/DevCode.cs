ebc0d670afcf47af0953cc73a2c2d5f749def19b
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-27 23:37:39 +0630

    Format Code

--- DotNet8WebApi.GenericRepositoryPatternExample.Shared/DevCode.cs
+++ DotNet8WebApi.GenericRepositoryPatternExample.Shared/DevCode.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Shared
+﻿namespace DotNet8WebApi.GenericRepositoryPatternExample.Shared;
+
+public static class DevCode
 {
-    public static class DevCode
+    public static bool IsNullOrEmpty(this string str)
     {
-        public static bool IsNullOrEmpty(this string str)
-        {
-            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-        }
+        return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
     }
 }
